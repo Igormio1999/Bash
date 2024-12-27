d0d7457c5bbc6eba8ef9f338ee20566e94118a5c
Igor <rixodape@gmail.com>
2024-12-27 23:50:04 +0300

    Скрипт для установки АСП на чистую машину

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
-#
-echo Hello world;
+#!/bin/bash
+
+# MAIN file script install Socit-common
+# 1 Step know UNIX OS
+# 2 Step check config file
+# 3 Step install 
+
+sourse ./config/main-config.sh
+
+echo ${IP_WEB_SERVER}
